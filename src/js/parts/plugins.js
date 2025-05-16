f68da8f2a0c8332a1b569f4fae0c281d059b76e2
varsha <varsha@karmadhi.com>
2025-05-16 17:36:03 +0530

    home page done

--- src/js/parts/plugins.js
+++ src/js/parts/plugins.js
@@ -3,7 +3,18 @@ import "slick-carousel";
 export class Plugins {
 
   init() {
+    this.TestimonialSlider();
+  }
 
+  TestimonialSlider(){
+    $('.testimonial-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:false,
+    });
   }
 
 }
